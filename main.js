c3957d68f430c4ace860beef745d317ed3aa0216
thegreatyamori <tod.jerson.morocho@gmail.com>
2023-05-26 10:01:06 -0500

    fix temporary port

--- main.js
+++ main.js
@@ -1,28 +1,29 @@
 const express = require("express");
 const cors = require("cors");
-const { exec } = require('child_process');
+const { exec } = require("child_process");
 
 const port = 3219;
-const host = '0.0.0.0';
+const host = "0.0.0.0";
 const app = express();
 app.use(cors());
 
 app.get("/", (req, res) => {
-    res.json({version: '0.0.1'});
+  res.json({ version: "0.0.1" });
 });
 
 app.get("/log", (req, res) => {
-    const message = `[${new Date().toUTCString()}]: ${req.query.content}`;
-    exec(`sh logger.sh "${message}"`, (error, stdout, _) => {
-        if (error) {
-            console.error(`exec error: ${error}`);
-            return;
-        }
+  const message = `[${new Date().toUTCString()}]: ${req.query.content}`;
+  exec(`sh logger.sh "${message}"`, (error, stdout, _) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
 
-        res.json({ log_message: "log created!" });
-    });
+    res.json({ log_message: "log created!" });
+  });
 });
 
-app.listen(process.ENV.PORT || port, host, () => {
-    console.log(`Server running on port ${port}`);
+app.listen(port, host, () => {
+  console.log(`${process.env}`);
+  console.log(`Server running on port ${port}`);
 });
